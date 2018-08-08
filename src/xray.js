7434a836d70af0884fcadb8bc701a14890b95414
Chad Jarreau <chad.jarreau@lifeomic.com>
2018-08-08 14:10:27 -0500

    fix build - linting errors
    fix build - linting errors

--- src/xray.js
+++ src/xray.js
@@ -1,11 +1,12 @@
 const xray = require('aws-xray-sdk-core');
 
-exports.captureAWS = function(awsSdk) {
-    return xray.captureAWS(awsSdk);
+exports.captureAWS = function (awsSdk) {
+  return xray.captureAWS(awsSdk);
 };
 
 exports.captureWithXRay = (awsSdk) => {
   xray.captureHTTPsGlobal(require('http'));
-  captureAWS(awsSdk);
+  const AWS = xray.captureAWS(awsSdk);
   xray.capturePromise();
+  return AWS;
 };
