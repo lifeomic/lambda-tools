dc00aae4fbdd8047cf8ccd209bd94126b4a490b8
Chad Jarreau <chad.jarreau@lifeomic.com>
2018-08-08 14:56:15 -0500

    remove unnecessary function, reorder for tidying up code
    remove unnecessary function, reorder for tidying up code

--- src/xray.js
+++ src/xray.js
@@ -1,12 +1,7 @@
 const xray = require('aws-xray-sdk-core');
 
-exports.captureAWS = function (awsSdk) {
-  return xray.captureAWS(awsSdk);
-};
-
-exports.captureWithXRay = (awsSdk) => {
+exports.captureWithXRay = function (awsSdk) {
   xray.captureHTTPsGlobal(require('http'));
-  const AWS = xray.captureAWS(awsSdk);
   xray.capturePromise();
-  return AWS;
+  return xray.captureAWS(awsSdk);
 };
