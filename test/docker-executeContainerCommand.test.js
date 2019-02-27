9318193a15c75530d4f6efc584178c9276b1e2b8
Zhenya Ragulin <zhenya.ragulin@lifeomic.com>
2019-02-27 10:37:25 -0500

    Stop container after test execution

--- test/docker-executeContainerCommand.test.js
+++ test/docker-executeContainerCommand.test.js
@@ -4,10 +4,15 @@ const {executeContainerCommand} = require('../src/docker');
 
 test('response includes stdout, stderr, and inspectOutput with ExitCode 0', async (t) => {
   const container = await createDefaultContainer();
-  await container.start();
-  const command = ['pwd'];
-  const {inspectOutput, stderr, stdout} = await executeContainerCommand({container, command});
-  t.is(stdout.toString('utf8'), '/\n');
-  t.is(stderr.toString('utf8'), '');
-  t.is(inspectOutput.ExitCode, 0);
+  try {
+    await container.start();
+    const command = ['pwd'];
+    const {inspectOutput, stderr, stdout} = await executeContainerCommand({container, command});
+    t.is(stdout.toString('utf8'), '/\n');
+    t.is(stderr.toString('utf8'), '');
+    t.is(inspectOutput.ExitCode, 0);
+  } finally {
+    // don't wait for container to stop since it could take a while
+    container.stop();
+  }
 });
