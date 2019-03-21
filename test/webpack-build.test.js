f6e30d897c273034410be71a376dd843f9054851
Matt Lavin <matt.lavin@lifeomic.com>
2019-03-21 07:38:05 -0400

    Adjust TSConfig testcase to new config structure

--- test/webpack-build.test.js
+++ test/webpack-build.test.js
@@ -6,6 +6,7 @@ const sinon = require('sinon');
 const test = require('ava');
 const uuid = require('uuid/v4');
 const {createLambdaExecutionEnvironment, destroyLambdaExecutionEnvironment, LambdaRunner} = require('../src/lambda');
+const find = require('lodash/find');
 
 const SUPPORTED_NODE_VERSIONS = ['6.10', '8.10'];
 
@@ -95,7 +96,7 @@ test('TSConfig files are supported', async (test) => {
   test.true(await fs.pathExists(path.join(test.context.buildDirectory, 'index.js.map')));
 
   const config = transformer.firstCall.args[0];
-  test.truthy(config.module.rules.find(rule => rule.loader === 'ts-loader'));
+  test.truthy(config.module.rules.find(rule => find(rule.use, { loader: 'ts-loader' })));
 });
 
 test('Typescript bundles are supported by default', async (test) => {
