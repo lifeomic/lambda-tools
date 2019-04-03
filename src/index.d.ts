05781fc648b135758c95a3bde2e11203c5545ed7
Charles Kenney <charlesc.kenney@gmail.com>
2019-04-03 13:36:37 -0400

    fix dynamodb helper typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,23 +1,21 @@
 import * as aws from "aws-sdk";
-import { TestInterface } from "ava";
 
-namespace dynamodb {
-  export interface DynamoDBContext {
+declare namespace dynamodb {
+  export interface Context {
     documentClient: aws.DynamoDB.DocumentClient;
     dynamoClient: aws.DynamoDB;
     streamsClient: aws.DynamoDBStreams;
   }
 
-  export interface DynamoDBHooks {
+  export interface Hooks {
     beforeAll(): Promise<void>;
-    beforeEach(): Promise<DynamoDBContext>;
-    afterEach(context: DynamoDBContext): Promise<void>;
+    beforeEach(): Promise<Context>;
+    afterEach(context: Context): Promise<void>;
     afterAll(): Promise<void>;
   }
 
   export function tableSchema(
     schema: ReadonlyArray<aws.DynamoDB.Types.CreateTableInput>
-  );
-  export function dynamoDBTestHooks(useUniqueTables: boolean);
-  export function useDynamoDB(test: TestInterface): void;
+  ): void;
+  export function dynamoDBTestHooks(useUniqueTables?: boolean): Hooks;
 }
