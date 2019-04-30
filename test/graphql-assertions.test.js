657209fc98a24ee38c811c064fd7746f1f532f45
Charles Kenney <charlesc.kenney@gmail.com>
2019-04-30 14:07:09 -0400

    bump @lifeomic/eslint-plugin-node

--- test/graphql-assertions.test.js
+++ test/graphql-assertions.test.js
@@ -81,7 +81,7 @@ test('assertError throws if no error matches the path', async (test) => {
 test('assertError throws if the error does not match the message', async (test) => {
   const query = '{ error }';
   const response = await test.context.graphql(query);
-  test.throws(() => assertError(response, 'error', 'some other message'), `'boom!' == 'some other message'`);
+  test.throws(() => assertError(response, 'error', 'some other message'), `'boom!' === 'some other message'`);
 });
 
 test('assertError throws if the path is undefined and no error has undefined path', test => {
