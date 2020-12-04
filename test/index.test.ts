e71a0259fb5ef432d2c9de3f6199e784827d66db
David Tanner <david.tanner@lifeomic.com>
2020-12-04 14:10:10 -0700

    Fix export types just to make them a little clearer.

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,13 +4,13 @@ import * as index from '../src';
 
 import * as docker from '../src/docker';
 import * as dynamodb from '../src/dynamodb';
-import { default as Environment } from '../src/Environment';
+import Environment from '../src/Environment';
 import * as graphql from '../src/graphql';
 import * as lambda from '../src/lambda';
 import * as localStack from '../src/localstack';
 import * as kinesis from '../src/kinesis';
 import * as mockServerLambda from '../src/mockServerLambda';
-import { default as WriteBuffer } from '../src/WriteBuffer';
+import WriteBuffer from '../src/WriteBuffer';
 
 test('exports match expected', t => {
   t.deepEqual(index, {
