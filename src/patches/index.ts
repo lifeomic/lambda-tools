6a5cafadf21dcdbba85db7c0a3fda30acdac4e84
Max Johansen <max.johansen@lifeomic.com>
2022-07-06 19:10:49 -0600

    fix: use `BannerPlugin` instead of `WrapperPlugin`

--- src/patches/index.ts
+++ src/patches/index.ts
@@ -1,30 +1,25 @@
 import assert from 'assert';
-import fs from 'fs-extra';
-import get from 'lodash/get';
 import path from 'path';
-const WrapperPlugin = require('wrapper-webpack-plugin');
+import { promises as fs } from 'fs';
+import { BannerPlugin } from 'webpack';
 
 const patches = {
-  dns: {
-    file: path.resolve(__dirname, 'dns.js'),
-    // DNS patches need to be inserted at the beginning of the bundle so that
-    // the DNS module can be updated before any other module loads it.
-    placement: 'header'
-  },
+  dns: async () => new BannerPlugin({
+    test: /\.js$/,
+    raw: true,
+    banner: await fs.readFile(path.resolve(__dirname, 'dns.js'), { encoding: 'utf8' }),
+  }),
+  lambda: async () => new BannerPlugin({
+    test: /\.js$/,
+    raw: true,
+    footer: true,
+    banner: await fs.readFile(path.resolve(__dirname, 'lambda.js'), { encoding: 'utf8' }),
+  }),
+} as const;
 
-  lambda: {
-    file: path.resolve(__dirname, 'lambda.js'),
-    placement: 'footer'
-  }
-};
-
-export const loadPatch = async (name: string) => {
-  const patch = get(patches, name);
+export const loadPatch = async (name: keyof typeof patches) => {
+  const patch = patches[name];
   assert(patch, `No patch found for '${name}'`);
 
-  return new WrapperPlugin({
-    test: /\.js$/,
-    // eslint-disable-next-line security/detect-non-literal-fs-filename
-    [patch.placement]: await fs.readFile(patch.file, { encoding: 'utf8' })
-  });
+  return await patch();
 };
